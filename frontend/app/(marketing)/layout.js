7b1c1c8a0371c68ea7a4ed24969d05df1e3cd2d5
Marwan Salem <marwansalem164@gmail.com>
2026-06-15 12:29:53 +0300

    Create route group for public pages

---
+++ frontend/app/(marketing)/layout.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/shared/Navbar";
+
+export default function MarketingLayout({ children }) {
+    return (
+        <>
+            <Navbar />
+            <main className="min-h-screen">{children}</main>
+        </>
+    );
+}
