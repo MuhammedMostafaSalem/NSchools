7f48cc94a768e98d745d00a585e29de4bd42a64e
Marwan Salem <marwansalem164@gmail.com>
2026-06-16 13:27:53 +0300

    Add FeaturedTeachers, TopCourses and Features components in home page

--- frontend/app/(marketing)/page.js
+++ frontend/app/(marketing)/page.js
@@ -1,9 +1,15 @@
 import HeroSection from "@/components/home/HeroSection";
+import FeaturedTeachers from "@/components/home/FeaturedTeachers";
+import TopCourses from "@/components/home/TopCourses";
+import Features from "@/components/home/Features";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <FeaturedTeachers />
+      <TopCourses />
+      <Features />
     </main>
   );
 }
