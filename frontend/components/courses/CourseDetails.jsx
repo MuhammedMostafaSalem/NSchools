6acef480a881488ac94287798f847b823f50b063
Marwan Salem <marwansalem164@gmail.com>
2026-06-16 21:15:58 +0300

    Create CourseDetails component

---
+++ frontend/components/courses/CourseDetails.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import banner from "@/public/assets/images/python-language-banner.jpg"
+import CourseDescription from "./CourseDescription"
+import CourseOutline from "./CourseOutline"
+import TeacherCourseDetails from "./TeacherCourseDetails"
+import CourseReviews from "./CourseReviews";
+
+const CourseDetails = () => {
+    return (
+        <main className="lg:col-span-8 space-y-8">
+            {/* Course Image */}
+            <section className="bg-white rounded-lg shadow-card overflow-hidden">
+                <Image
+                    src={banner}
+                    alt="Python Course"
+                    className="w-full h-[300px] md:h-[350px] lg:h-[440px] object-cover"
+                />
+            </section>
+
+            {/* Description */}
+            <CourseDescription />
+
+            {/* Teacher */}
+            <TeacherCourseDetails />
+
+            {/* Course Outline */}
+            <CourseOutline />
+
+            {/* Reviews */}
+            <CourseReviews />
+        </main>
+    )
+}
+
+export default CourseDetails
