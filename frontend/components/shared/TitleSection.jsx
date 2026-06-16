e8d1a66fc9e28857bda5460d6e5d25bf2501735f
Marwan Salem <marwansalem164@gmail.com>
2026-06-16 16:11:07 +0300

    Remove path

--- frontend/components/shared/TitleSection.jsx
+++ frontend/components/shared/TitleSection.jsx
@@ -1,18 +1,10 @@
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 
-const TitleSection = ({nameTitle, path}) => {
+const TitleSection = ({nameTitle}) => {
     return (
         <div className="flex justify-between items-center mb-8">
-            <h2 className="text-2xl font-bold text-secondary-dark">{nameTitle}</h2>
-            {
-                path === "" ? null : (
-                    <Link href={path} className="text-secondary-light hover:text-secondary transition flex gap-2 items-center text-[16px]">
-                        <div>المزيد</div>
-                        <IoIosArrowBack />
-                    </Link>
-                )
-            }
+            <h2 className="text-xl md:text-2xl font-bold text-secondary-dark">{nameTitle}</h2>
         </div>
     )
 }
