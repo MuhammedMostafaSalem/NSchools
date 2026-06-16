6c85bfe960b257fbadf42513b004fae67c7ed1dc
Marwan Salem <marwansalem164@gmail.com>
2026-06-16 13:28:29 +0300

    Add footer component

--- frontend/app/(marketing)/layout.js
+++ frontend/app/(marketing)/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 
 export default function MarketingLayout({ children }) {
@@ -5,6 +6,7 @@ export default function MarketingLayout({ children }) {
         <>
             <Navbar />
             <main className="min-h-screen">{children}</main>
+            <Footer />
         </>
     );
 }
