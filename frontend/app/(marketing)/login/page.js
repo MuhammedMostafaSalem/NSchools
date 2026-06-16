247bcfb92a0d3c28c4293f26831fdc1142d164e4
Marwan Salem <marwansalem164@gmail.com>
2026-06-17 01:49:17 +0300

    Create Login page

---
+++ frontend/app/(marketing)/login/page.js
@@ -0,0 +1,17 @@
+import LoginForm from "@/components/Auth/LoginForm";
+
+export default function Login() {
+    return (
+        <main className="min-h-screen auth-bg-image bg-background-alt flex items-center justify-center px-4">
+            <div className="w-full max-w-md mt-[120px]">
+                <h1 className="text-center text-4xl font-bold text-secondary-dark mb-6">
+                    تسجيل الدخول
+                </h1>
+                <div className="bg-white rounded-lg shadow-card p-6">
+
+                    <LoginForm />
+                </div>
+            </div>
+        </main>
+    )
+}
