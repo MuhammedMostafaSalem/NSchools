18ff00d34215d19be47dd7ae62edcbb0adb3a535
Marwan Salem <marwansalem164@gmail.com>
2026-06-19 17:16:10 +0300

    Add oath

--- frontend/components/shared/TitleSection.jsx
+++ frontend/components/shared/TitleSection.jsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 
-const TitleSection = ({nameTitle}) => {
+const TitleSection = ({ title, path }) => {
     return (
-        <div className="flex justify-between items-center mb-8">
-            <h2 className="text-xl md:text-2xl font-bold text-secondary-dark">{nameTitle}</h2>
+        <div className={`
+            flex items-center mb-8
+            ${path ? "justify-between" : "justify-center"}
+        `}>
+            <h2 className="section-title">{title}</h2>
+            {
+                path ? <Link href={path}>عرض الكل</Link> : null
+            }
         </div>
     )
 }
